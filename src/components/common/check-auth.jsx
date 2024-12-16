a02ddac695c9d024d22aad7db6a4a504f5293299
asifaowadud <asifaowadud@gmail.com>
2024-12-17 02:00:33 +0600

    route protection lowered

--- src/components/common/check-auth.jsx
+++ src/components/common/check-auth.jsx
@@ -1,4 +1,4 @@
-// import { Navigate, useLocation } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 
 // function CheckAuth({ isAuthenticated, user, children }) {
 //   const location = useLocation();
